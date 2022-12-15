c93d2d5648073fadd8d801e7bacb72d7f6ed30cf
c0dject <creseeds@gmail.com>
2022-12-15 19:16:56 +0900

    Modify: subhome v.1 1차 완료 - category_img_url 컬럼 null 처리(하위카테고리 때문) - res에 이미지 링크 추가

--- src/models/subhomeDao.ts
+++ src/models/subhomeDao.ts
@@ -6,6 +6,7 @@ const getSubhomeList = async () => {
         SELECT id AS category_id,
                category,
                introduction,
+               category_img_url,
                IFNULL(parent_category_id, "상위 카테고리") AS parent_category_id
         FROM category c
         WHERE parent_category_id IS NULL
