78318649e340cc7ee0772a684fbb4209c7bcea90
Jisoo <k3ji0706@gmail.com>
2022-12-16 17:32:15 +0900

    DELETE : 주석 삭제

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,9 +1,6 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
-// import aws from 'aws-sdk';
 import { S3Client } from '@aws-sdk/client-s3';
-
-// aws.config.loadFromPath(__dirname + '/../config/s3.json');
 
 const upload = multer({
   storage: multerS3({
