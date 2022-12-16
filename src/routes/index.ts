82d883f12e5f056d0b75b9cf74d24024ab2e5c45
Jisoo <k3ji0706@gmail.com>
2022-12-16 16:32:36 +0900

    FIX : rebase 후 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,12 +6,13 @@ import userRouter from './userRouter';
 import categoryRouter from './categoryRouter';
 import subhomeRouter from './subhomeRouter';
 import feedListRouter from './feedListRouter';
+import feedRouter from './feedRouter';
 
 router.use('/user', userRouter);
 router.use('/category', categoryRouter);
 router.use('/subhome', subhomeRouter);
 router.use('/feedlist', feedListRouter);
-
+router.use(errHandler);
 router.use('/feed', feedRouter);
 
 export default router;
