5d85ef455d58eeab3373b9ee21ab032d5ce348ac
c0dject <creseeds@gmail.com>
2022-12-21 10:13:54 +0900

    Add: feedList API 페이지네이션 추가

--- src/services/feedListService.ts
+++ src/services/feedListService.ts
@@ -3,16 +3,26 @@ import feedListDao from '../models/feedListDao';
 const getFeedList = async (
   location_id?: number,
   category_id?: number,
-  sub_category_id?: number
+  sub_category_id?: number,
+  page?: number
 ) => {
-  let selectFilters: string;
+  const limit = 4;
 
+  if (!page) {
+    page = 1;
+  }
+  const pageOffset: number = (page - 1) * limit;
+  const pagenation = `
+  LIMIT ${pageOffset}, ${limit}
+  `;
+
+  let selectFilters: string;
   if (location_id && category_id && sub_category_id) {
     selectFilters = `
       WHERE l.id = ${location_id}
       AND ca.id =  ${sub_category_id}
     `;
-    return await feedListDao.getFeedList(selectFilters);
+    return await feedListDao.getFeedList(selectFilters, pagenation);
   }
 
   if (location_id && category_id) {
@@ -20,17 +30,17 @@ const getFeedList = async (
       WHERE l.id = ${location_id}
       AND (ca.id = ${category_id} OR ca.parent_category_id = ${category_id})
       `;
-    return await feedListDao.getFeedList(selectFilters);
+    return await feedListDao.getFeedList(selectFilters, pagenation);
   }
 
   if (location_id) {
     selectFilters = `
       WHERE l.id = ${location_id}
       `;
-    return await feedListDao.getFeedList(selectFilters);
+    return await feedListDao.getFeedList(selectFilters, pagenation);
   }
   selectFilters = '';
-  return await feedListDao.getFeedList(selectFilters);
+  return await feedListDao.getFeedList(selectFilters, pagenation);
 };
 
 export default { getFeedList };
